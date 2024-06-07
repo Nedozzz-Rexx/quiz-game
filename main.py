8d4fff5a81c711ce54c46d2202647e09933a9928
Nada Abbas Mohamed Magzoub <unicorn@Nadas-MacBook-Air.local>
2024-06-07 18:42:46 +0400

    Modified the question style options

--- main.py
+++ main.py
@@ -15,15 +15,28 @@ def main():
         menu.display()
         choice = menu.get_choice()
         if choice == "1":
-            question_type = input("Choose question type (multiple, boolean): ").lower()
+            question_type = None
             while question_type not in ["multiple", "boolean"]:
-                print("Invalid choice. Please choose 'multiple' or 'boolean'.")
-                question_type = input("Choose question type (multiple, boolean): ").lower()
+                print("Choose question type:")
+                print("1. Multiple Choice Questions (MCQ)")
+                print("2. True or False (T or F)")
+                print("3. Go back to the main menu")
+                question_type_choice = input("Your choice: ")
+                if question_type_choice == "1":
+                    question_type = "multiple"
+                elif question_type_choice == "2":
+                    question_type = "boolean"
+                elif question_type_choice == "3":
+                    break  # Go back to the main menu
+                else:
+                    print("Invalid choice. Please choose 1, 2, or 3.")
+                    continue
 
-            questions_data = get_random_questions(question_type)
-            questions = [Question(q['question'], q['incorrect_answers'] + [q['correct_answer']], q['correct_answer'], q['difficulty']) for q in questions_data]
-            quiz_game = QuizGame(questions, score_manager)
-            quiz_game.play()
+            if question_type:
+                questions_data = get_random_questions(question_type)
+                questions = [Question(q['question'], q['incorrect_answers'] + [q['correct_answer']], q['correct_answer'], q['difficulty']) for q in questions_data]
+                quiz_game = QuizGame(questions, score_manager)
+                quiz_game.play()
         elif choice == "2":
             score_manager.display_high_scores()
         elif choice == "3":
